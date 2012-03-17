5012fc2129ed4998ec3b20e6709c7d30828d5e65
Michele Melis <melismichele@gmail.com>
2012-03-17 18:19:45 +0100

    fix message model

--- db/migrate/20120317111222_create_messages.rb
+++ db/migrate/20120317111222_create_messages.rb
@@ -1,9 +1,15 @@
 class CreateMessages < ActiveRecord::Migration
-  def change
+  def self.up
     create_table :messages do |t|
-      t.text :content
+      t.string :content
+      t.integer :user_id
 
       t.timestamps
     end
+    add_index :messages, [:user_id, :created_at]
+  end
+
+  def self.down
+    drop_table :messages
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,10 +14,13 @@
 ActiveRecord::Schema.define(:version => 20120317152553) do
 
   create_table "messages", :force => true do |t|
-    t.text     "content"
+    t.string   "content"
+    t.integer  "user_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  add_index "messages", ["user_id", "created_at"], :name => "index_messages_on_user_id_and_created_at"
 
   create_table "users", :force => true do |t|
     t.string   "name"
