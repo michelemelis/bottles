fd79b2de0c835b86d324edbf26a10396f1ce381c
michelemelis <melismichele@gmail.com>
2012-03-17 13:40:16 +0100

    added test and model validation for Message

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,5 @@
 class Message < ActiveRecord::Base
+  
+  validates :content, :presence => true
+  
 end
--- test/unit/message_test.rb
+++ test/unit/message_test.rb
@@ -4,4 +4,10 @@ class MessageTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  test "message attributes must not be empty" do
+    message = Message.new
+    assert message.invalid?
+    assert message.errors[:content].any?
+  end
 end
